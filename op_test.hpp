a28e2ff294c11849aec3bc712c7ee37533434b38
amiddendorp22 <amiddendorp22@gmail.com>
2020-10-31 22:11:09 -0700

    Added working functions and some tests for Op, Pow, and Sub.

--- op_test.hpp
+++ op_test.hpp
@@ -6,8 +6,32 @@
 #include "op.hpp"
 
 TEST(OpTest, OpEvaluateNonZero) {
-    Op* test = new Op(8);
-    EXPECT_EQ(test->evaluate(), 8);
+    Op* test = new Op(7.5);
+    EXPECT_EQ(test->evaluate(), 7.5);
+}
+
+TEST(OpTest, OpEvaluateNegative)
+{
+	Op* test = new Op(-3);
+	EXPECT_EQ(test->evaluate(), -3);
+}
+
+TEST(OpTest, OpStringifyNonZero)
+{
+	Op* test = new Op(3);
+	EXPECT_EQ(test->stringify(),"3.000000");
+}
+
+TEST(OpTest, OpEvaluateZeros)
+{
+	Op* test = new Op(0);
+	EXPECT_EQ(test->evaluate(), 0);
+}
+
+TEST(OpTest, OpStringifyZeros)
+{
+	Op* test = new Op(0);
+	EXPECT_EQ("0.000000",test->stringify());
 }
 
 #endif //__OP_TEST_HPP__
